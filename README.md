6fc7c99209b32b730c9af395422d25376986f83b
Haroon Mahmood <70590766+haroon-mahmood-4276@users.noreply.github.com>
2024-11-25 12:10:56 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# OpenWithNautilus
+A bash scripts to add extra "Open With" functionality in Nautilus
+
+## Install Sublime Editor Extension
+
+### Install Extension
+
+```
+wget -qO- https://raw.githubusercontent.com/haroon-mahmood-4276/OpenWithNautilus/main/sublime-install.sh | bash
+```
+
+### Uninstall Extension
+
+```
+rm -f ~/.local/share/nautilus-python/extensions/sublime-nautilus.py
+```
+
+## Install VSCode Editor Extension
+
+### Install Extension
+
+```
+wget -qO- https://raw.githubusercontent.com/haroon-mahmood-4276/OpenWithNautilus/main/vscode-install.sh | bash
+```
+
+### Uninstall Extension
+
+```
+rm -f ~/.local/share/nautilus-python/extensions/vscode-nautilus.py
+```
+
+## Install Zed Editor Extension
+
+### Install Extension
+
+```
+wget -qO- https://raw.githubusercontent.com/haroon-mahmood-4276/OpenWithNautilus/main/zed-install.sh | bash
+```
+
+### Uninstall Extension
+
+```
+rm -f ~/.local/share/nautilus-python/extensions/zed-nautilus.py
+```
+
+## 🚀 Note
+
+Extension for VSCode is taken from [Harry-cpp/code-nautils](https://github.com/harry-cpp/code-nautilus/). All credit goes to the original creater. 
---
+++ sublime-nautilus.py
@@ -0,0 +1,62 @@
+# Sublime Nautilus Extension
+#
+# Place me in ~/.local/share/nautilus-python/extensions/,
+# ensure you have python-nautilus package, restart Nautilus, and enjoy :)
+#
+# This script is released to the public domain.
+
+from gi.repository import Nautilus, GObject
+from subprocess import call
+import os
+
+# path to vscode
+SUBLIME = 'subl'
+
+# what name do you want to see in the context menu?
+SUBLIMENAME = 'Sublime'
+
+# always create new window?
+NEWWINDOW = False
+
+
+class SublimeExtension(GObject.GObject, Nautilus.MenuProvider):
+
+    def launch_vscode(self, menu, files):
+        safepaths = ''
+        args = ''
+
+        for file in files:
+            filepath = file.get_location().get_path()
+            safepaths += '"' + filepath + '" '
+
+            # If one of the files we are trying to open is a folder
+            # create a new instance of vscode
+            if os.path.isdir(filepath) and os.path.exists(filepath):
+                args = '--new-window '
+
+        if NEWWINDOW:
+            args = '--new-window '
+
+        call(SUBLIME + ' ' + args + safepaths + '&', shell=True)
+
+    def get_file_items(self, *args):
+        files = args[-1]
+        item = Nautilus.MenuItem(
+            name='SublimeOpen',
+            label='Open in ' + SUBLIMENAME,
+            tip='Opens the selected files with Sublime'
+        )
+        item.connect('activate', self.launch_vscode, files)
+
+        return [item]
+
+    def get_background_items(self, *args):
+        file_ = args[-1]
+        item = Nautilus.MenuItem(
+            name='SublimeOpenBackground',
+            label='Open in ' + SUBLIMENAME,
+            tip='Opens the current directory in Sublime'
+        )
+        item.connect('activate', self.launch_vscode, [file_])
+
+        return [item]
